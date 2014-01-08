c0131fd1600d95777240cab5f8dd2357cd6794c1
davidwu <davidwudv@gmail.com>
2014-01-08 09:50:40 +0800

    version1.0

---
+++ newtabdialog.cpp
@@ -0,0 +1,25 @@
+﻿#include "newtabdialog.h"
+
+NewTabDialog::NewTabDialog(QWidget *parent) :
+    QDialog(parent)
+{
+    dialog->setupUi(this);
+    connect(dialog->OKButton, SIGNAL(clicked()), this, SLOT(OKButtonClicked()));
+    connect(dialog->CancelButton, SIGNAL(clicked()), this, SLOT(CancelButtonClicked()));
+}
+
+NewTabDialog::~NewTabDialog()
+{
+    delete dialog;
+}
+
+void NewTabDialog::OKButtonClicked()
+{
+    nameOfNewTab = dialog->lineEdit->text();
+    done(ClickedResult::OK);
+}
+
+void NewTabDialog::CancelButtonClicked()
+{
+    done(ClickedResult::Cancel);
+}
---
+++ newtabdialog.h
@@ -0,0 +1,31 @@
+﻿#ifndef CREATENEWTABDIALOG_H
+#define CREATENEWTABDIALOG_H
+
+#include <QDialog>
+#include "ui_CreateNewTabDialog.h"
+
+class NewTabDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit NewTabDialog(QWidget *parent = 0);
+    ~NewTabDialog();
+    enum ClickedResult
+    {
+        OK,
+        Cancel
+    };
+
+signals:
+
+public slots:
+    void OKButtonClicked();
+    void CancelButtonClicked();
+
+private:
+    QString nameOfNewTab;
+    Ui::CreateNewTabDialog *dialog;
+
+};
+
+#endif // CREATENEWTABDIALOG_H
